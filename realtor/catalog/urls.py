4b50dc0cc84f33a8fdd38b92be9829979b98854d
Barnabas Kutassy <kutassy.b@gmail.com>
2018-03-04 21:37:43 +0100

    Adding stuff

--- realtor/catalog/urls.py
+++ realtor/catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('items/', views.PropertyListView.as_view(), name='items'),
 
 ]
--- realtor/catalog/views.py
+++ realtor/catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import generic
 
+from .models import Property, City, Heating, PropertyState, PropertyType
 # Create your views here.
+def index(request):
+    """
+    View function for home page of site.
+    """
+    num_of_props = Property.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context ={'num_of_props': num_of_props}
+    )
+
+class PropertyListView(generic.ListView):
+    model = Property
+    context_object_name = 'prop_list'   # your own name for the list as a template variable
+    template_name = 'prop_list.html'  # Specify your own template name/location
